568b1cdd61064d27492326d6beb69bc36e2019aa
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-10-19 20:50:30 -0400

    update

--- C0766112_F2019_MID_TERM_MAD3004/main.swift
+++ C0766112_F2019_MID_TERM_MAD3004/main.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-var bill1 = Internet(billId: 101, billDate: "12-12-2018", billType: .Internet, billAmount: 45.0, providerName: "Rodgers", internetUsed: 12)
-var bill2 = Mobile(billId: 105, billDate: "05-06-2019", billType: .Mobile, billAmount: 55, mobileManufacturer: "Samsung Inc.", plan: "Internet + Talk", mobileNum: 1234567890, internetUsed: 4, minutesUsed: 120)
-var bill3 = Hydro(billId: 110, billDate: "13-06-2019", billType: .Hydro, billAmount: 150, agency: "Ontario electricity Services Inc.", unitsConsumed: 230)
+ var bill1 = Internet(billId: 101, billDate: "12-12-2018", billType: .Internet, billAmount: 45.0, providerName: "Rodgers", internetUsed: 12)
+ var bill2 = Mobile(billId: 105, billDate: "05-06-2019", billType: .Mobile, billAmount: 55, mobileManufacturer: "Samsung Inc.", plan: "Internet + Talk", mobileNum: 1234567890, internetUsed: 4, minutesUsed: 120)
+ var bill3 = Hydro(billId: 110, billDate: "13-06-2019", billType: .Hydro, billAmount: 150, agency: "Ontario electricity Services Inc.", unitsConsumed: 230)
 //var bill1 = Bill(billId: <#T##Int#>, billDate: <#T##String#>, billType: <#T##Bill.TypeofBills#>, billAmount: <#T##Float#>)
 var cust1 = Customer(customerId: 5002, firstName: "Charan", lastName: "Kaur", emailId: "charankaur123@ymail.com")
 var cust2 = Customer(customerId: 5003, firstName: "Preet", lastName: "Sikka", emailId: "preetsikka321@gmail.com")
