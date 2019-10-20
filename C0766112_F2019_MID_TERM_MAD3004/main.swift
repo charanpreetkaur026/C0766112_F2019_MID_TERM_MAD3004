66d32b73028de444d698432035160ac2077fc8c4
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-10-19 20:07:46 -0400

    display by customer ID

--- C0766112_F2019_MID_TERM_MAD3004/main.swift
+++ C0766112_F2019_MID_TERM_MAD3004/main.swift
@@ -32,11 +32,14 @@ addCustomer(cust: cust1)
 addCustomer(cust: cust3)
 addCustomer(cust: cust2)
 func getByCustomerId(customerId: Int){
+    print("\n ~~~~~~~~~~~~~ GETTING DETAILS BY CUSTOMER ID~~~~~~~~~~~~~~~~\n ")
     if custDictionary.keys.contains(customerId)
     {
         custDictionary[customerId]?.display()
     }else{
-        print("This Customer not found")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Customer not found")
+         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     }
     
 }
